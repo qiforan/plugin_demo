be30fd89cb1a9243e86f24990aa7e3402183c999
GitHub Action <action@github.com>
2021-01-05 07:24:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/qiforan/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/qiforan/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.qiforan.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.qiforan.plugindemo.MyBundle
 
 class MyApplicationService {
 
